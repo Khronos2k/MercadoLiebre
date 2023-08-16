9358729eb6a15559f863945b9cf84aec24e311c4
Khronos2k <silver_spear20@hotmail.com>
2023-08-16 18:41:08 -0300

    add: script

--- src/app.js
+++ src/app.js
@@ -5,9 +5,8 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{
-    console.log('Corriendo servidor en puerto 3000');
-});
+const port = process.envPORT || 3001;
+app.listen(port, ()=>console.log(`Corriendo servidor en puerto ${port}`));
 
 app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
